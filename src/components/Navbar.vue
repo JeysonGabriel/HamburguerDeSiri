cecdcff19fbb19b923281250de9144afa6eb7f17
JeysonGabriel <jeysongoliveira204@gmail.com>
2024-02-28 00:20:42 -0300

    primeiras configurações

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="navbar">
+        <ul class="opcoes">
+            <li>Contato</li>
+            <li>Sobre</li>
+            <li>Home</li>
+            <li>Pedir</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+    .navbar{
+        height: 10rem;
+        width: 100%;
+        background-color: rgb(55, 117, 211);
+    }
+    .opcoes{
+        
+        color: black;
+    }
+
+    </style>
